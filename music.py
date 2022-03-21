3c044813745512ee2c7bdfcf2b01b9f0d710e9cb
Dev kumar <55612982+Devkumar22@users.noreply.github.com>
2022-03-22 00:28:45 +0530

    webcam

--- music.py
+++ music.py
@@ -1,6 +1,20 @@
 import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+import av
+
+class EmotionProcessor:
+    def recv(self, frm):
+        frame = frm.to_ndarray(format="bgr24")
+
+        return av.VideoFrame.from_ndarray(frame, format="bgr24")
 
 lang = st.text_input("Language")
 singer = st.text_input("singer")
 
+if lang and singer:
+    webrtc_streamer(key="key", desired_playing_state=True,
+    video_processor_factory=EmotionProcessor)
+
 btn = st.button("Recommend me songs")
+
+
