aaa0fa866ac6351c62972475ff62b3256929b14b
pradhanharshit <55655048+pradhanharshit@users.noreply.github.com>
2022-04-30 22:21:18 +0530

    Update login-signup.py
    3rd commit

--- apps/login-signup.py
+++ apps/login-signup.py
@@ -29,3 +29,8 @@ def create_usertable():
 def add_userdata(username,password):
 	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
 	conn.commit()
+
+def login_user(username,password):
+	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
+	data = c.fetchall()
+	return data
